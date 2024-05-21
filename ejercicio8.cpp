5f1df80f9f6f9d400d04bbda18d21928d8c8276a
rpresno <rjpresno@gmail.com>
2024-05-21 01:38:56 -0300

    Ejercicio 8 DAC, cumple

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -5,8 +5,51 @@
 
 using namespace std;
 
+int triosDAC(int* arr, int i, int f){
+    
+    int largo = f - i + 1;
+    int resto = largo % 3;
+    
+    //casos base
+    assert(resto != 0 && largo > 0);
+
+    if (largo < 3){
+        return arr[i];
+    }
+
+
+    //divide inicial (resto >= 0)
+    int medio = largo/2 + i;
+    
+    int corte = medio - 2;
+    int anterior;
+
+    //veo donde hay un corte
+    do {
+        anterior = corte;
+        corte++;
+    } while (arr[corte] == arr[anterior]);
+
+    if (corte % 3 == 0){
+        return triosDAC(arr,corte,f);
+    }
+    else { 
+        return triosDAC(arr,i,anterior);
+    }
+        
+
+}
+
+
+
 int main()
 {
-    // TODO
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cout << triosDAC(arr, 0, n-1);
     return 0;
 }
