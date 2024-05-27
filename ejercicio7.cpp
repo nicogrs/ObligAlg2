e6077788155be56408c1e7a11112e69a329bac53
Nicolas <nicogrs20@gmail.com>
2024-05-27 20:38:00 -0300

    Se sube ejercicio7 que compila y funciona Ok

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -2,16 +2,52 @@
 #include <string>
 #include <iostream>
 #include <limits>
-#include "MaxHeap.h"
-#include "MaxHeapImp.cpp"
+#include "tads/MaxHeapImp.cpp"
+#include "tads/MaxHeap.h"
 
 using namespace std;
 
+int comparar(int a, int b){
+    return a - b;
+}
+
+
+
 int main()
 {
-    MaxHeapImp<int> *v1 = new MaxHeapImp<int>(5,comparar);
-};
+    int pSistema;
+    cin >> pSistema;
 
-int comparar(int a, int b){
-    return b < a ? a : b;
+    int n;
+    cin >> n;
+
+    MaxHeap<int>* virus = new MaxHeapImp<int>(n,comparar);
+
+    for (int i = 0; i < n; i++){
+        int pot;
+        cin >> pot;
+
+        if (pot <= pSistema){
+            pSistema += pot;
+        }
+        else {
+            virus->insert(pot);
+        }
+    }
+
+    while (!virus->isEmpty()){
+        int pot = virus->peek();
+        virus->removePeek();
+
+        if (pot <= pSistema){
+            pSistema += pot;
+        }
+        else {
+            return false;
+        }
+    }
+
+    return true;
+
+    return 0;
 }
