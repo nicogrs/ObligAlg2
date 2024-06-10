11b6e3122606f5b2ed0b51bd55e8e603312d5caf
rpresno <rjpresno@gmail.com>
2024-06-10 01:03:58 -0300

    ej 9 en proceso

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -5,8 +5,211 @@
 
 using namespace std;
 
+//Resolucion usando memoizacion
+/*
+Para llegar a la casilla (x-1,y-1):
+1) Calculo los primos de 1 al mayor de x o y, guardo en array
+2) Hago matriz de X x Y, inicializada en -1, que indica formas de llegar a cada casilla.
+3) Iniciando de (x-1,y-1) calculo formas:
+    - en una casilla (a,b) es la suma de las formas llegando desde arriba + las formas llegando desde abajo
+    - las formas de cada uno de los lados es, sumando cada primo menor a a y b respectivamente, ej:
+        --Llegando desde arriba: (b cte)
+            /1/  formas(a-1,b) +
+            /2/  formas(a-2,b) +
+            /3/  formas(a-2,b) +
+            /5/  formas(a-5,b) + .....
+            /p/  formas(a-p,b) para todo p < a. p > 0.
+        --Llegando desde izquierda idem pero simetrico
+
+NOTAS:
+    -si a == b, la matriz es simetrica
+    -estando en la seccion "cuadrada" de la matriz tambien
+
+
+*/
+
+int* primosErastotenes(int n){
+    cout << "primos hasta " << n;
+    assert(n > 0);
+
+    bool* criba = new bool[n+1](); //cada indice es el numero
+    for (int i = 0; i <= n; i++){
+        criba[i] = true; //cada numero inicia como primo y se va tachando
+    }
+    criba[0] = false;
+    criba[1] = false;
+    
+    int cantPrimos = 0;
+
+    for(int i = 2; i <= n; i++){
+        if (criba[i]){ //si es primo
+            cantPrimos++;
+
+            //cada multiplo de ese primo "lo tacho", no asi al primo
+            for (int j = 2*i; j <= n; j += i){
+                criba[j] = false;
+            }
+        }
+    }
+
+    //Luego hago un array solo de primos
+    int* primos = new int[cantPrimos]();
+    int actual = 0;
+    cout << "primos: ";
+    for (int i = 0; i <= n; i++){
+        if (criba[i]){
+            primos[actual] = i;
+            actual++;
+            cout << " " << i;
+        }
+    }
+    cout << ".";
+
+    delete[] criba;
+    return primos;
+
+}
+
+int COLUMNAS = 20;
+int FILAS = 20;
+
+int** initMatriz()
+{
+    int** tab = new int *[FILAS]();
+    for (int f = 0; f < FILAS; f++)
+    {
+        tab[f] = new int[COLUMNAS]();
+        for (int c = 0; c < COLUMNAS; c++)
+        {
+            tab[f][c] = 0;
+        }
+
+    }
+    return tab;
+}
+
+int** initMatriz(int inicial)
+{
+    int** tab = new int *[FILAS]();
+    for (int f = 0; f < FILAS; f++)
+    {
+        tab[f] = new int[COLUMNAS]();
+        for (int c = 0; c < COLUMNAS; c++)
+        {
+            tab[f][c] = inicial;
+        }
+
+    }
+    return tab;
+}
+
+
+
+int ** cantCaminosTab(int * primos, int cantPrimos)
+{
+    int **matriz = initMatriz();
+    matriz[0][0] = 1;
+    for (int i = 0; i < cantPrimos ; i++)
+    {
+        int paso = primos[i];
+        for (int c = 1; c < COLUMNAS; c++)
+        {
+        if((c - paso -1 >= 0))
+        matriz[0][c] += matriz[0][c-paso-1] + 1; //+ matriz[0][c-paso-1] ;   
+        }
+    }
+    for (int j = 0; j < cantPrimos ; j++)
+    {
+        int paso = primos[j];
+        for (int f = 1; (f - paso >= 0) && f < FILAS; f++)
+        {
+        matriz[f][0] += matriz[f-paso][0]; //+ matriz[f-paso-1][0] ;   
+        }
+    }
+    return matriz;
+/*     // "paso recursivo"
+    for (int f = 1; f < FILAS; f++)
+    {
+        for (int c = 1; c < COLUMNAS; c++)
+        {
+            
+            matriz[f][c] = matriz[f - 1][c] + matriz[f][c - 1];
+        }
+    }
+    return matriz[FILAS - 1][COLUMNAS - 1];
+
+    int paso = primos[i];
+    matriz[f][c] = matriz[f - paso][c] + matriz[f][c-paso];
+    
+        for (int i = 0; (f - paso >= 0) && (c - paso >= 0);i++)
+            int paso = primos[i];
+            matriz[f][c] = matriz[f - paso][c] + matriz[f][c-paso]; */
+
+}
+
+/* int formasRec(int** &matriz, int f, int c, int* primos){
+    
+    if (f < 0 || c < 0)
+        return 0;
+
+    if (matriz[f][c] != -1)
+        return matriz[f][c];
+    
+    int res = 0;
+    int i = 0;
+    //caso sobre borde izq, solo "miro para arriba"
+    if (c <= 1){
+        //recorro cada primo
+        while (i < sizeof(primos) && f - primos[i] >= 0){
+            res += formasRec(matriz, f - primos[i], c, primos);
+            i++;
+        }
+    }
+    else
+
+    //caso sobre fila superior, miro "a la izquierda"
+    if (f <= 1){
+        i = 0;
+        //recorro cada primo
+        while (i < sizeof(primos) && c - primos[i] >= 0 ){
+            res += formasRec(matriz, f, c - primos[i], primos);
+            i++;
+        }
+    }
+    else
+
+    //caso en el medio
+    i = 0;
+    //recorro cada primo
+    while (i < sizeof(primos) && (c - primos[i] >= 0 || f - primos[i] >= 0 )){
+        res += formasRec(matriz, f - primos[i], c, primos);
+        res += formasRec(matriz, f, c - primos[i], primos);
+        i++;
+    }
+
+    matriz[f][c] = res;
+    return res;
+} */
+
+
+void imprimirMatriz(int ** matriz)
+{
+    for (int f = 0; f < FILAS; f++)
+    {   
+        cout << "" << endl;
+        for (int c = 0; c < COLUMNAS; c++)
+        {
+            cout << "[ "<< matriz[f][c] << " ]" << " ";
+        }
+        
+    }
+}
+
 int main()
 {
-    // TODO
+
+    int * primos = primosErastotenes(20);
+    int ** mat = cantCaminosTab(primos, sizeof(primos));
+    imprimirMatriz(mat);
     return 0;
 }
